f9e92e2d70c18e0b97a8d7779eede76296bebec2
tugce <tugcetopkaccerit@gmail.com>
2023-07-08 20:59:40 -0400

    fixing error

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config()
 const express = require('express')
+const path = require('path');
 const app = express()
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
@@ -22,6 +23,7 @@ app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
 app.use('/', profilesController)
 app.use('/users', usersController)
